af31f012d7c0cd1adef3255097a7152438c9c0ba
sanvi02 <115354633+sanvi02@users.noreply.github.com>
2023-10-08 16:56:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+/*
+
+//Eg 1
+
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//Eg 2
+
+let scor = null
+
+console.log(typeof scor);
+console.log(typeof(scor));
+
+let valueInNumbe = Number(scor)
+console.log(typeof valueInNumbe);
+console.log(valueInNumbe);
+
+//Eg 3
+
+let sco = true //if sco is false then output will be shown as 0
+
+console.log(typeof sco);
+console.log(typeof(sco));
+
+let valueInNumb = Number(sco)
+console.log(typeof valueInNumb);
+console.log(valueInNumb);
+
+//Eg 4
+let sc = null
+
+console.log(typeof sc);
+console.log(typeof(sc));
+
+let valueInNum = Number(sc)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+
+//"33" = 33
+//"33abc" = Nan but type is number
+// true = 1 ; false = 0
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "sanika" = true
+// "" = false
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
