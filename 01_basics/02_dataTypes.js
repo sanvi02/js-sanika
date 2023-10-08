6d56d97a403ad8322e9f3beefbabcba1ea6b4178
sanvi02 <115354633+sanvi02@users.noreply.github.com>
2023-10-08 16:30:27 +0000

    commit

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,6 +2,28 @@
 
 //alert( 3 + 3 ) //we are using nodeJs, not browser 
 
-console.log( 3 + 3 )
+console.log( 3 +
+     3 )
 
 console.log("sanika")
+//code readability should be high
+
+let name = "sanika"
+let age = 21
+let isLoggedIn = false
+
+
+// number = 2 to power 53
+// bigint 
+// string = " " or ' '
+// boolean = true/false
+// null = standalone value (null can also be assigned) if you asked for temperature and answer is not given then its null
+// undefined = 
+// symbol = unique
+
+// object
+
+console.log(typeof"sanika");
+console.log(typeof null ); //null is object
+console.log(typeof undefined); //undefined
+
