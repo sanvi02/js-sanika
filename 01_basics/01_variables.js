77259c6ac174e6e9166996350833fea7432e7544
sanvi02 <115354633+sanvi02@users.noreply.github.com>
2023-10-08 15:00:16 +0000

    he

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,22 @@
-const accountId= "144545"
+const accountId= "144545" //if value assigns once they cant be change
 let accountEmail= "sanika@gmail.com"
-var accountPasswordv = "12345"
+var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState; // if value is not assigned to variable then it will show the undefined as output
 
-//accountId = 2
 
-console.log(accountId);
+//accountId = 2 //not allowed
+
+accountEmail ="dghahgd@gmail.com"
+accountPassword ="243672723673"
+accountCity = "mumbai"
+
+
+console.log(accountId); 
+
+/* 
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState]);
+
