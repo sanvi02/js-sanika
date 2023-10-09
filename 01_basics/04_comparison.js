7e44f672a5e887ce6e84b7b319290fe804d536df
sanvi02 <115354633+sanvi02@users.noreply.github.com>
2023-10-09 17:47:12 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+/* console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2<=1);
+console.log(2!=1); */
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  its checks datatype also so output is false
+ console.log("2"===2);
+
