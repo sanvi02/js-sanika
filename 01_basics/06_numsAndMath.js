738c4f3b1690bb64e73885c868d30eeb22118099
sanvi02 <115354633+sanvi02@users.noreply.github.com>
2023-10-12 18:20:46 +0000

    numsAndMath

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,37 @@
+/*
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.88787
+console.log(otherNumber.toPrecision(3)); //learn more
+
+const thirdNumber = 10000000
+console.log(thirdNumber.toLocaleString('en-IN')); // makes number 1,00,00,000
+
+///++++++++++++++++++++++++++++++++++++++++++MATHs++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.3));
+console.log(Math.min(2,6,1,3,6,0));
+console.log(Math.max(1,2,5,8,4,3));
+*/
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*10 + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*(Math.floor(Math.random () * (max - min + 1 )+ min)));
+
+
